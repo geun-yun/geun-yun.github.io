1e28f68aa4a66c628722051e5b1d45a24792d4bd
geun-yun <118499721+geun-yun@users.noreply.github.com>
2025-09-19 15:52:42 +0000

    Deploying to gh-pages from @ geun-yun/geun-yun.github.io@a6814d81b970b64876d82273da94a3f28561eb33 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,16 +440,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-i-am-officially-graduated-from-australian-national-university-with-bachelor-of-advanced-computing-research-and-development-honours",
+          title: 'I am officially graduated from Australian National University with Bachelor of Advanced Computing...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
