3be987790c3bf5c1ae067040f9c84770e8574db0
geun-yun <118499721+geun-yun@users.noreply.github.com>
2025-09-19 16:25:12 +0000

    Deploying to gh-pages from @ geun-yun/geun-yun.github.io@04af5e6caa356820533187ff7732925d8397e1cb 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -51,13 +51,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/early-research/";
           },
-        },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/people/";
-          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
