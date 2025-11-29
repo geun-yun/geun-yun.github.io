8de1a2162f4e272397c4606cac7e49b0fc3b691b
geun-yun <118499721+geun-yun@users.noreply.github.com>
2025-11-29 02:17:49 +0000

    Deploying to gh-pages from @ geun-yun/geun-yun.github.io@c7c9efb3f6e561599ed584afe868a7bc7b36882d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -51,17 +51,7 @@ ninja.data = [{
           section: "News",},{id: "news-i-will-be-joining-the-university-of-sydney-from-december-to-february-through-the-vacation-research-internship-program-to-work-on-fair-play-in-machine-learning-tackling-bias-in-data",
           title: 'I will be joining the University of Sydney from December to February through...',
           description: "",
-          section: "News",},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
+          section: "News",},{id: "projects-project-4",
           title: 'project 4',
           description: "another without an image",
           section: "Projects",handler: () => {
